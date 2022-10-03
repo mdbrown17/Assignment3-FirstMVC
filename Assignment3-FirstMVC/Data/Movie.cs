cee0af3648ec47974007cbe74d5bd601077f2750
Matt Brown <mdbrown17@crimson.ua.edu>
2022-10-03 10:50:04 -0500

    Updated landing page and added movie class

---
+++ Assignment3-FirstMVC/Data/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment3_FirstMVC.Data
+{
+    public class Movie
+    {
+        public int Id { get; set; } 
+        public string? Title { get; set; }
+        public string? ImdbHyperlink { get; set; }
+        public string? Genre { get; set; }
+        public int? RealeaseYear { get; set; }
+        public string? Poster { get; set; }
+    }
+}
